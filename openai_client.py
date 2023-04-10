1d65926fc7e2eb50ad9bf1c206461857b8c58da3
indragit <indragit@gmail.com>
2023-04-10 13:00:38 +0900

    ファイルシテイ

--- openai_client.py
+++ openai_client.py
@@ -1,29 +1,9 @@
 import openai
-import argparse
-import json
 import os
-import uuid
-
-# openai.api_key を環境変数から読み込む
-openai.api_key = os.environ["OPENAI_API_KEY"]
-
-def create_history_file(file_path):
-    initial_history = {"messages": [{"role": "system", "content": "You are a helpful assistant."}]}
-    with open(file_path, "w") as f:
-        json.dump(initial_history, f, ensure_ascii=False, indent=2)
-    print(f"Created a new history file: {file_path}")
-
-def load_history(file_path):
-    if not os.path.exists(file_path):
-        create_history_file(file_path)
-        
-    with open(file_path, "r") as f:
-        history = json.load(f)
-    return history["messages"]
-
-def save_history(file_path, history):
-    with open(file_path, "w") as f:
-        json.dump({"messages": history}, f, ensure_ascii=False, indent=2)
+import json
+import argparse
+from datetime import datetime
+from pathlib import Path
 
 def generate_text(prompt, model, history):
     response = openai.ChatCompletion.create(
@@ -36,22 +16,40 @@ def generate_text(prompt, model, history):
     )
     return response.choices[0].message['content'].strip()
 
+def load_history(history_path):
+    if history_path.exists():
+        with open(history_path, "r") as f:
+            history = json.load(f)
+    else:
+        history = [{"role": "system", "content": "You are a helpful assistant."}]
+        with open(history_path, "w") as f:
+            json.dump(history, f)
+        print(f"Created a new history file: {history_path.name}")
+    return history
+
+def save_history(history_path, history):
+    with open(history_path, "w") as f:
+        json.dump(history, f)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate text using OpenAI API.")
-    parser.add_argument("--model", default="gpt-3.5-turbo", type=str, help="Name of the model to use.")
-    parser.add_argument("--prompt", default="What is the capital of France?", type=str, help="The text prompt to generate text from.")
+    parser = argparse.ArgumentParser(description="OpenAI API Client for GPT-3.5-turbo")
+    parser.add_argument("--model", type=str, default="gpt-3.5-turbo", help="Model name")
+    parser.add_argument("--prompt", type=str, required=True, help="Prompt text")
+    parser.add_argument("--history_file", type=str, help="Path to the history file")
+
     args = parser.parse_args()
 
-    unique_id = str(uuid.uuid4())
-    history_file = f"history_{unique_id}.json"
-    history = load_history(history_file)
-    generated_text = generate_text(args.prompt, args.model, history)
-    
-    # Update the conversation history with the user's input and model's response
-    history.append({"role": "user", "content": args.prompt})
-    history.append({"role": "assistant", "content": generated_text})
-    
-    # Save the updated history to the file
-    save_history(history_file, history)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
+    if args.history_file:
+        history_path = Path("history") / args.history_file
+    else:
+        history_path = Path("history") / f"history_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
+
+    history = load_history(history_path)
+    generated_text = generate_text(args.prompt, args.model, history)
+    history.append({"role": "assistant", "content": generated_text})
+    save_history(history_path, history)
+
+    print("Generated text:")
     print(generated_text)
